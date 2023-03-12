82735b9e06d2df6b6d83840177609d10a17e2f18
tuchsanai <tuchsanai@gmail.com>
2023-03-12 23:42:54 +0700

    z

--- Lab5/main.py
+++ Lab5/main.py
@@ -1,61 +1,51 @@
-from fastapi import FastAPI,Query
+
+import uvicorn
+from fastapi import FastAPI
 from pydantic import BaseModel
-
-
-from fastapi import  File, UploadFile
-from typing import List, Dict
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-
-
-
+import cv2
+import base64
 
 app = FastAPI()
 
-
-# Assuming the book data is stored as a list of 10 dictionaries
-books = [
-    {"id": 1, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald", "year": 1925},
-    {"id": 2, "title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960},
-    {"id": 3, "title": "1984", "author": "George Orwell", "year": 1949},
-    {"id": 4, "title": "Brave New World", "author": "Aldous Huxley", "year": 1932},
-    {"id": 5, "title": "The Catcher in the Rye", "author": "J.D. Salinger", "year": 1951},
-    {"id": 6, "title": "Pride and Prejudice", "author": "Jane Austen", "year": 1813},
-    {"id": 7, "title": "The Hobbit", "author": "J.R.R. Tolkien", "year": 1937},
-    {"id": 8, "title": "The Lord of the Rings", "author": "J.R.R. Tolkien", "year": 1954},
-    {"id": 9, "title": "The Hitchhiker's Guide to the Galaxy", "author": "Douglas Adams", "year": 1979},
-    {"id": 10, "title": "The Shining", "author": "Stephen King", "year": 1977},
-]
+class ImageRequest(BaseModel):
+    image: str
+    name: str
+    surname: str
+    numbers: List[int]
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# encode image as base64 string
+def encode_image(image):
+    _, encoded_image = cv2.imencode(".jpg", image)
+    return "data:image/jpeg;base64," + base64.b64encode(encoded_image).decode()
+
+# decode base64 string to image
+def decode_image(image_string):
+    encoded_data = image_string.split(',')[1]
+    nparr = np.frombuffer(base64.b64decode(encoded_data), np.uint8)
+    return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+def apply_canny(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray, 100, 200)
+    return edges
+
+
+@app.post("/process-image")
+async def process_image(image_request: ImageRequest):
+    image = decode_image(image_request.image)
+    edges = apply_canny(image)
+    processed_image = encode_image(edges)
+
+    return {"name": image_request.name,
+            "surname": image_request.surname,
+            "numbers": image_request.numbers,
+            "processed_image": processed_image}
 
 
 
-## Path parameters
-@app.get("/book/{book_id}")
-async def get_book(book_id: int):
-    return books[book_id-1]
 
 
-
-# Quary parameters
-@app.get("/search")
-def search_books(title: str = Query(None), author: str = Query(None)):
-    """
-    Returns a list of books that match the search criteria.
-    """
-    # Filter the list of books by title and/or author
-    books_filtered = books
-    if title is not None:
-        books_filtered = [b for b in books_filtered if title.lower() in b["title"].lower()]
-    if author is not None:
-        books_filtered = [b for b in books_filtered if author.lower() in b["author"].lower()]
-
-    # Return the filtered list of books
-    return {"books": books_filtered}
 
 
